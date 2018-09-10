7047b3e97cddc7f52d7f113c4a8bdf9215e0d569
Irfan Charania <irfan.charania@gmail.com>
2018-09-09 22:27:28 -0700

    Add filter for src
    To account for empty image tags

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def update_links(content, page_theme, page_links):
         h['href'] = build_link_url(h['href'], page_theme, page_links)
 
     ''' removing srcset=True filter to catch lxml screwups '''
-    for i in soup.findAll('img'):
+    for i in soup.findAll('img', src=True):
         i['src'] = build_img_url(i['src'])
 
     return soup.prettify(formatter="html").strip()
